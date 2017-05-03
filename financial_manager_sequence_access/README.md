c31a7f7c776df0a620d9352916783fb1d3ec5315
ADomingo <albert.domingo@rgbconsulting.com>
2017-05-03 16:14:00 +0200

    [ADD] Added financial_manager_sequence_access

---
+++ financial_manager_sequence_access/README.md
@@ -0,0 +1,23 @@
+Financial Manager Sequence Access
+=================================
+
+Add permisions to financial manager group to allow to edit sequences.
+
+
+Credits
+=======
+
+License
+-------
+
+* [GNU Affero General Public License] (http://www.gnu.org/licenses/agpl.html)
+
+Author
+------
+
+* Copyright, RGB Consulting SL (www.rgbconsulting.com)
+
+Contributors
+------------
+
+* RGB Consulting SL <odoo@rgbconsulting.com>
---
+++ financial_manager_sequence_access/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Financial Manager Sequence Access
+#   Copyright 2017 RGB Consulting, SL
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Financial Manager Sequence Access",
+    'version': '8.0.0.1.0',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'author': "RGB Consulting SL",
+    'website': "http://www.rgbconsulting.com",
+    'category': 'Account',
+    'summary': """Financial Manager Sequence Access""",
+    'description': """
+Financial Manager Sequence Access
+=================================
+Add permisions to financial manager group to allow to edit sequences.
+""",
+
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+
+    'demo': [
+    ],
+
+    'qweb': [
+    ],
+}
